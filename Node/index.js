89e65aa0fa21499e11edf81694151457b410dc95
Witold Hajkowicz <witold.hajkowicz@student.put.poznan.pl>
2024-05-23 01:20:53 +0200

    Add sessions

--- Node/index.js
+++ Node/index.js
@@ -1,5 +1,6 @@
 const { fetchData } = require('./database')
 const express = require('express');
+const session = require('express-session');
 const {readFile} = require('fs').promises;
 const app = express();
 const port = 3000;
@@ -11,8 +12,14 @@ app.use(express.json(limit = '1mb'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
-let shoppingCart = [];
+// let shoppingCart = [];
 let items = [];
 let message = 'Welcome to the store!';
 
@@ -23,7 +30,6 @@ app.get('/', async (req, res) => {
 
         res.render('pages/index.ejs', {items: items, message: message});
         message = 'Welcome to the store!';
-        console.log('shoppingCart:', shoppingCart);
     } catch (err) {
         res.send('Error');
     }
@@ -31,11 +37,11 @@ app.get('/', async (req, res) => {
 
 app.get('/checkout', async (req, res) => {
     try {
+        const shoppingCart = req.session.shoppingCart || [];
         const cardItems = shoppingCart.map(itemId => {
             console.log('itemId:', itemId);
             const item = items.find((item) => (item.id == itemId));
             console.log(item);
-            console.log('items:', items[0].id);
             return {
                 id: item.id,
                 name: item.name,
@@ -55,6 +61,8 @@ app.post('/addToCart', (req, res) => {
     const itemId = req.body.itemId;
     // You can perform further validations here, e.g., if the item exists in the database
 
+    const shoppingCart = req.session.shoppingCart || [];
+
     // Add the item ID to the shopping cart
     if (!shoppingCart.includes(itemId)) {
         shoppingCart.push(itemId);
@@ -63,12 +71,14 @@ app.post('/addToCart', (req, res) => {
 
     // Send a response indicating success
     // res.status(200).send('Item added to cart successfully');
+    req.session.shoppingCart = shoppingCart;
     res.redirect('/');
 });
 
 app.post('/removeFromCart', (req, res) => {
     console.log(req.body);
     const itemId = req.body.itemId;
+    let shoppingCart = req.session.shoppingCart;
     // You can perform further validations here, e.g., if the item exists in the database
 
     shoppingCart = shoppingCart.filter(id => id !== itemId);
@@ -76,13 +86,15 @@ app.post('/removeFromCart', (req, res) => {
 
     // Send a response indicating success
     //res.status(200).send('Item removed from cart successfully');
+    req.session.shoppingCart = shoppingCart;
     res.redirect('/checkout')
 });
 
 app.post('/cancelCheckout', (req, res) => {
-    console.log('Checkout cancelled:', shoppingCart);
-    shoppingCart = [];
-    res.status(200).send('Checkout cancelled successfully');
+    console.log('Checkout cancelled:', req.session.shoppingCart);
+    req.session.shoppingCart = [];
+    // res.status(200).send('Checkout cancelled successfully');
+    res.redirect('/');
     message = 'Checkout cancelled successfully';
 });
 
